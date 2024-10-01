2e1f7ba03567c5487990558b9ac59e66b5dbed33
UzielLH <014430576@ulsaoaxaca.edu.mx>
2024-10-01 17:21:42 -0600

    Update TripulacionServiceImplements.java

--- src/main/java/com/edu/mx/lasalle/oaxaca/servicio_aeropuerto/service/implement/TripulacionServiceImplements.java
+++ src/main/java/com/edu/mx/lasalle/oaxaca/servicio_aeropuerto/service/implement/TripulacionServiceImplements.java
@@ -4,10 +4,50 @@
  */
 package com.edu.mx.lasalle.oaxaca.servicio_aeropuerto.service.implement;
 
+import com.edu.mx.lasalle.oaxaca.servicio_aeropuerto.models.TripulacionModel;
+import com.edu.mx.lasalle.oaxaca.servicio_aeropuerto.repositories.TripulacionRepository;
+import com.edu.mx.lasalle.oaxaca.servicio_aeropuerto.service.TripulacionService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 /**
  *
  * @author tokay
  */
-public class TripulacionServiceImplements {
+@Service
+public class TripulacionServiceImplements implements TripulacionService{
+    @Autowired
+    private TripulacionRepository tripulacionRepository;
+    
+    @Override
+    public void registrarTripulacion(TripulacionModel tripulacionModel) {
+        tripulacionRepository.save(tripulacionModel);
+    }
+
+    @Override
+    public List<TripulacionModel> obtenerTripulacion() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public TripulacionModel getTripulacion(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void actualizarDatosTripulacion(TripulacionModel tripulacionModel, int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void borrarTripulacion(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void borrarTodosTripulacion() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
