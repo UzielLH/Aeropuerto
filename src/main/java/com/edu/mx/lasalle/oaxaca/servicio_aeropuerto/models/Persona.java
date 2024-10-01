6814ba15556378feca687a9193518e4f21b15926
UzielLH <014430576@ulsaoaxaca.edu.mx>
2024-10-01 16:29:46 -0600

    Create Persona.java
    Interfaz Persona

---
+++ src/main/java/com/edu/mx/lasalle/oaxaca/servicio_aeropuerto/models/Persona.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.edu.mx.lasalle.oaxaca.servicio_aeropuerto.models;
+
+/**
+ *
+ * @author tokay
+ */
+public interface Persona {
+    public void viajar();
+    public void abordar();
+    public void dormir();
+    public void checkIn();
+    public void irBano();
+    public void comer();
+}
