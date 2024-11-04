7a010378220c45c258bb35cebf6b4c778536b4ea
danielpraisethelord <danielyosef777@gmail.com>
2024-11-04 17:34:48 -0600

    Cors

--- src/main/java/com/edu/mx/lasalle/oaxaca/servicio_aeropuerto/config/CorsConfig.java
+++ src/main/java/com/edu/mx/lasalle/oaxaca/servicio_aeropuerto/config/CorsConfig.java
@@ -1,24 +1,25 @@
-package com.edu.mx.lasalle.oaxaca.servicio_aeropuerto.config;
+// package com.edu.mx.lasalle.oaxaca.servicio_aeropuerto.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.cors.CorsConfiguration;
+// import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-@Configuration
-public class CorsConfig {
+// @Configuration
+// public class CorsConfig {
 
-    @Bean
-    public UrlBasedCorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(false);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
+// @Bean
+// public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+// CorsConfiguration config = new CorsConfiguration();
+// config.setAllowCredentials(false);
+// config.addAllowedOrigin("*");
+// config.addAllowedHeader("*");
+// config.addAllowedMethod("*");
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-        return source;
-    }
+// UrlBasedCorsConfigurationSource source = new
+// UrlBasedCorsConfigurationSource();
+// source.registerCorsConfiguration("/**", config);
+// return source;
+// }
 
-}
+// }
